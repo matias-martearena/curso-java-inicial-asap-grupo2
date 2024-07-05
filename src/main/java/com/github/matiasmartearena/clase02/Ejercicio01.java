b78fabd1a7585cdc34a685d87051e3a442b0043c
Matias Martearena <mmartearena@asap-consulting.net>
2024-07-05 10:40:31 -0300

    Clase02 - Ejercicio01

---
+++ src/main/java/com/github/matiasmartearena/clase02/Ejercicio01.java
@@ -0,0 +1,23 @@
+package com.github.matiasmartearena.clase02;
+
+import ar.edu.ort.p1.util.Consola;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+
+		int numero = Consola.leerEntero("Introduce un numero entero...");
+		int primer = 0; // Primer numero de la secuencia
+		int segundo = 1; // Segundo numero de la secuencia
+
+		System.out.print("Los " + numero + " primeros números de la serie de Fibonacci son: ");
+
+		for (int i = 0; i < numero; i++) {
+			System.out.print(primer + " "); // Imprime el primer numero de la secuencia
+
+			int resultado = primer + segundo;
+			primer = segundo;
+			segundo = resultado;
+		}
+	}
+}
