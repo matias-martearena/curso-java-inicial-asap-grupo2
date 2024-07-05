8ced9daa7476a53f1aab2d7c3d0091ccc412217d
Matias Martearena <mmartearena@asap-consulting.net>
2024-07-05 10:05:39 -0300

    Ejercicio04

---
+++ src/main/java/com/github/matiasmartearena/clase01/Ejercicio04.java
@@ -0,0 +1,17 @@
+package com.github.matiasmartearena.clase01;
+
+import ar.edu.ort.p1.util.Consola;
+
+public class Ejercicio04 {
+	
+	public static void main(String[] args) {
+		
+		int year = Consola.leerEntero("Ingrese un año...");
+
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			System.out.println("Es bisiesto");
+		} else {
+			System.out.println("No es bisiesto");
+		}
+	}
+}
