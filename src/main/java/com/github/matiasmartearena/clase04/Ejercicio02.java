5c27d3741bf1a0eef94efc6b49446028bdb335dc
Matias Martearena <mmartearena@asap-consulting.net>
2024-07-16 13:26:51 -0300

    Clase4- Ejercicio02

--- src/main/java/com/github/matiasmartearena/clase04/Ejercicio02.java
+++ src/main/java/com/github/matiasmartearena/clase04/Ejercicio02.java
@@ -1,9 +1,40 @@
 package com.github.matiasmartearena.clase04;
 
-//import java.util.Scanner;
-
 public class Ejercicio02 {
+
 	public static void main(String[] args) {
-		System.out.print("Ejercicio 2");
+
+		final int CANT_ELEMENTOS = 8;
+		int numerosAleatorios[] = new int[CANT_ELEMENTOS];
+
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			numerosAleatorios[i] = (int) (Math.random() * 10 + 1);
+		}
+
+		// Muestra el array original
+		System.out.print("\nArray desordenado: ");
+
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			System.out.print(numerosAleatorios[i] + " ");
+		}
+
+		// Algoritmo de ordenamiento burbuja
+		for (int i = 0; i < numerosAleatorios.length - 1; i++) {
+			for (int j = 0; j < (numerosAleatorios.length - 1 - i); j++) {
+				if (numerosAleatorios[j] > numerosAleatorios[j + 1]) {
+					int aux = numerosAleatorios[j];
+					numerosAleatorios[j] = numerosAleatorios[j + 1];
+					numerosAleatorios[j + 1] = aux;
+				}
+			}
+		}
+
+		// Muestra el array ordenado
+		System.out.print("\nArray ordenado: ");
+
+		for (int i = 0; i < numerosAleatorios.length; i++) {
+			System.out.print(numerosAleatorios[i] + " ");
+		}
+
 	}
 }
