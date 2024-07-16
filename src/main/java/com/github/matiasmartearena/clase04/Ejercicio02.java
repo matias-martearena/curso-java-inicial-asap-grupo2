15b02ddf10be4e9d13c92f41e2e20c99557a7d67
Matias Martearena <mmartearena@asap-consulting.net>
2024-07-16 11:59:27 -0300

    Clase04 - Ejercicio01

---
+++ src/main/java/com/github/matiasmartearena/clase04/Ejercicio02.java
@@ -0,0 +1,9 @@
+package com.github.matiasmartearena.clase04;
+
+//import java.util.Scanner;
+
+public class Ejercicio02 {
+	public static void main(String[] args) {
+		System.out.print("Ejercicio 2");
+	}
+}
